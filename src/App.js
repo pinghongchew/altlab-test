d7b532d3799c2062d756d39cbee8b89383a47c28
Pinghong Chew <pinghong94@gmail.com>
2021-09-13 23:12:11 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
 import logo from './logo.svg';
 import './App.css';
+import { Container, Nav, Button, Row, Col, Image, Navbar } from 'react-bootstrap';
+import goldenBurger from './image/burger-gold.jpeg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div style={{ backgroundImage: "linear-gradient(to top right, #5ae7f2, #a6f9ff)", padding: "1rem" }}>
+        <Navbar>
+          <Container>
+            <Navbar.Brand href="#home">Logo</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Features</Nav.Link>
+              <Nav.Link href="#features">Traits</Nav.Link>
+              <Nav.Link href="#pricing">Roadmap</Nav.Link>
+              <Nav.Link href="#pricing">Team</Nav.Link>
+              <Nav.Link href="#pricing">Lore</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+        <Container fluid="lg">
+          <Row className="justify-content-md-center">
+            <Col xs={12} md={5} style={{ padding: "1.75rem" }}>
+              <h2>Welcome to Metallic Burger</h2>
+              <h3>10000 randomly generated 3D characters aping around the Ethereum blockchain as ERC-721 tokens and hosted on IPFS.</h3>
+              <p>The collection drops on 2021. The price of each Ape will be 0.02 ETH.</p>
+            </Col>
+            <Col xs={12} md={5} style={{ padding: "1.75rem" }}>
+              <Image fluid src={goldenBurger}></Image>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      <div style={{ backgroundImage: "linear-gradient(to top right, #75ff83, #d6ffda)", padding: "1rem" }}>
+        <Container fluid="lg">
+          <Row className="justify-content-md-center">
+            <Col xs={12} md={5} style={{ padding: "1.75rem" }}>
+              <h2>Welcome to Metallic Burger</h2>
+              <h3>10000 randomly generated 3D characters aping around the Ethereum blockchain as ERC-721 tokens and hosted on IPFS.</h3>
+              <p>The collection drops on 2021. The price of each Ape will be 0.02 ETH.</p>
+            </Col>
+            <Col xs={12} md={5} style={{ padding: "1.75rem" }}>
+              <Image fluid src={goldenBurger}></Image>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      <div style={{ backgroundImage: "linear-gradient(to top right, #75ff83, #d6ffda)", padding: "1rem" }}>
+        <Container fluid="lg">
+          <Row className="justify-content-md-center">
+            <Col xs={12} md={5} style={{ padding: "1.75rem" }}>
+              <h2>Welcome to Metallic Burger</h2>
+              <h3>10000 randomly generated 3D characters aping around the Ethereum blockchain as ERC-721 tokens and hosted on IPFS.</h3>
+              <p>The collection drops on 2021. The price of each Ape will be 0.02 ETH.</p>
+            </Col>
+            <Col xs={12} md={5} style={{ padding: "1.75rem" }}>
+              <Image fluid src={goldenBurger}></Image>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      <div style={{ backgroundImage: "linear-gradient(to top right, #75ff83, #d6ffda)", padding: "1rem" }}>
+        <Container fluid="lg">
+          <Row className="justify-content-md-center">
+            <Col xs={12} md={5} style={{ padding: "1.75rem" }}>
+              <h2>Welcome to Metallic Burger</h2>
+              <h3>10000 randomly generated 3D characters aping around the Ethereum blockchain as ERC-721 tokens and hosted on IPFS.</h3>
+              <p>The collection drops on 2021. The price of each Ape will be 0.02 ETH.</p>
+            </Col>
+            <Col xs={12} md={5} style={{ padding: "1.75rem" }}>
+              <Image fluid src={goldenBurger}></Image>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </div>
   );
 }
