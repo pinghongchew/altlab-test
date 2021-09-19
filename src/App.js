93aecee9ee87a04fae0409f6e5b9fdea500aa097
Pinghong Chew <pinghong94@gmail.com>
2021-09-19 17:17:51 +0800

    burgur lab style

--- src/App.js
+++ src/App.js
@@ -1,80 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
-import { Container, Nav, Button, Row, Col, Image, Navbar } from 'react-bootstrap';
 import goldenBurger from './image/burger-gold.jpeg';
 
 function App() {
+  const styleMain = {
+    backgroundImage: "url(https://myburgerlab.com/static/img/home/img_hero_3_desktop.jpg)",
+    // backgroundPosition: "left -200px bottom 10px",
+    height: "650px"
+  }
+
+  const styleRarity = {
+    backgroundColor: "rgb(29, 29, 29)",
+    height: "650px"
+  }
+
   return (
-    <div>
-      <div style={{ backgroundImage: "linear-gradient(to top right, #5ae7f2, #a6f9ff)", padding: "1rem" }}>
-        <Navbar>
-          <Container>
-            <Navbar.Brand href="#home">Logo</Navbar.Brand>
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Features</Nav.Link>
-              <Nav.Link href="#features">Traits</Nav.Link>
-              <Nav.Link href="#pricing">Roadmap</Nav.Link>
-              <Nav.Link href="#pricing">Team</Nav.Link>
-              <Nav.Link href="#pricing">Lore</Nav.Link>
-            </Nav>
-          </Container>
-        </Navbar>
-        <Container fluid="lg">
-          <Row className="justify-content-md-center">
-            <Col xs={12} md={5} style={{ padding: "1.75rem" }}>
-              <h2>Welcome to Metallic Burger</h2>
-              <h3>10000 randomly generated 3D characters aping around the Ethereum blockchain as ERC-721 tokens and hosted on IPFS.</h3>
-              <p>The collection drops on 2021. The price of each Ape will be 0.02 ETH.</p>
-            </Col>
-            <Col xs={12} md={5} style={{ padding: "1.75rem" }}>
-              <Image fluid src={goldenBurger}></Image>
-            </Col>
-          </Row>
-        </Container>
-      </div>
-      <div style={{ backgroundImage: "linear-gradient(to top right, #75ff83, #d6ffda)", padding: "1rem" }}>
-        <Container fluid="lg">
-          <Row className="justify-content-md-center">
-            <Col xs={12} md={5} style={{ padding: "1.75rem" }}>
-              <h2>Welcome to Metallic Burger</h2>
-              <h3>10000 randomly generated 3D characters aping around the Ethereum blockchain as ERC-721 tokens and hosted on IPFS.</h3>
-              <p>The collection drops on 2021. The price of each Ape will be 0.02 ETH.</p>
-            </Col>
-            <Col xs={12} md={5} style={{ padding: "1.75rem" }}>
-              <Image fluid src={goldenBurger}></Image>
-            </Col>
-          </Row>
-        </Container>
-      </div>
-      <div style={{ backgroundImage: "linear-gradient(to top right, #75ff83, #d6ffda)", padding: "1rem" }}>
-        <Container fluid="lg">
-          <Row className="justify-content-md-center">
-            <Col xs={12} md={5} style={{ padding: "1.75rem" }}>
-              <h2>Welcome to Metallic Burger</h2>
-              <h3>10000 randomly generated 3D characters aping around the Ethereum blockchain as ERC-721 tokens and hosted on IPFS.</h3>
-              <p>The collection drops on 2021. The price of each Ape will be 0.02 ETH.</p>
-            </Col>
-            <Col xs={12} md={5} style={{ padding: "1.75rem" }}>
-              <Image fluid src={goldenBurger}></Image>
-            </Col>
-          </Row>
-        </Container>
-      </div>
-      <div style={{ backgroundImage: "linear-gradient(to top right, #75ff83, #d6ffda)", padding: "1rem" }}>
-        <Container fluid="lg">
-          <Row className="justify-content-md-center">
-            <Col xs={12} md={5} style={{ padding: "1.75rem" }}>
-              <h2>Welcome to Metallic Burger</h2>
-              <h3>10000 randomly generated 3D characters aping around the Ethereum blockchain as ERC-721 tokens and hosted on IPFS.</h3>
-              <p>The collection drops on 2021. The price of each Ape will be 0.02 ETH.</p>
-            </Col>
-            <Col xs={12} md={5} style={{ padding: "1.75rem" }}>
-              <Image fluid src={goldenBurger}></Image>
-            </Col>
-          </Row>
-        </Container>
-      </div>
-    </div>
+    <>
+      <section class="bg-center bg-no-repeat bg-cover" style={styleMain}>
+        <div class="container mx-auto h-full">
+          <div class="text-white grid grid-rows-3 grid-cols-2 grid-flow-col gap-4 py-32">
+            <div class="row-span-1 col-span-1 m-8">
+              <h1 class="text-6xl">Welcome to ALT Burger</h1>
+            </div>
+            <div class="row-span-1 mx-8">
+              <h2 class="text-2xl">10000 randomly generated 3D characters aping around the Ethereum blockchain as ERC-721 tokens and hosted on IPFS.</h2>
+            </div>
+            <div class="row-span-1 mx-8">
+              <button class="border rounded-lg inline-block px-8 py-2">Mint</button>
+            </div>
+            <div class="place-self-center row-span-3 col-span-1 m-8">
+              <video class="max-w-xs" src="https://chibiapes.com/assets/videos/video.mp4" autoPlay loop muted></video>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section style={styleRarity}>
+        <div class="text-white container mx-auto h-full">
+          <h2 class="py-16 text-3xl mx-8">Your ALT Burger Rarity</h2>
+          <div class="grid grid-flow-row grid-cols-4 gap-4 mx-8 h-96">
+            <div class="border fade-in-mode-up opacity-0">
+              FRESHLY BAKED BAMBOO CHARCOAL BUNS
+            </div>
+            <div class="border fade-in-mode-up animation-delay-1 opacity-0">
+              FRESHLY BAKED BAMBOO CHARCOAL BUNS
+            </div>
+            <div class="border fade-in-mode-up animation-delay-2 opacity-0">
+              FRESHLY BAKED BAMBOO CHARCOAL BUNS
+            </div>
+            <div class="border fade-in-mode-up animation-delay-3 opacity-0">
+              FRESHLY BAKED BAMBOO CHARCOAL BUNS
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
 
