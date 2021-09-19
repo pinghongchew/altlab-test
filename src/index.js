874932068ddd2e93f7358e4434ee1a0599c772ae
Pinghong Chew <pinghong94@gmail.com>
2021-09-19 18:07:41 +0800

    added title

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Helmet } from 'react-helmet'
 
 ReactDOM.render(
   <React.StrictMode>
+    <Helmet>
+      <title>ALT Burger</title>
+    </Helmet>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
